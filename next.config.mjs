7a8ad1af168e3c9c0f2fb3a1d1cc362c24272592
alerodriguezn <navarrocmo@gmail.com>
2024-09-23 22:34:17 -0600

    fix:closing tag

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,22 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.scdn.co", // Este es el que ya tenías configurado
+      },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com", // Añade este dominio
+      },
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+      },
+    ],
+  },
+};
 
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'i.scdn.co', // Este es el que ya tenías configurado
-        },
-        {
-          protocol: 'https',
-          hostname: 'via.placeholder.com', // Añade este dominio
-        },
-       {
-                protocol:"https",
-                hostname:"image.tmdb.org"
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+export default nextConfig;
--- src/app/(home)/movies/page.tsx
+++ src/app/(home)/movies/page.tsx
@@ -13,5 +13,6 @@ export default function MoviesPage() {
     <div className="w-1/2 mx-auto  ">
 
     </div>
+    </div>	
   );
 }
